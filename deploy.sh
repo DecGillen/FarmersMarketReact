7af314c8baa54ec614165f74bcb1aa1572fc2c2d
D Gillen <declangillen@live.com>
2023-08-09 22:48:33 +0100

    tried group add fix

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Add this line to get the GID of the Docker group on the host
+DOCKER_GROUP_GID=$(stat -c '%g' /var/run/docker.sock)
+
 CURRENT_INSTANCE=$(sudo docker ps -a -q --filter ancestor="$IMAGE_NAME" --format="{{.ID}}")
 
 if [ -n "$CURRENT_INSTANCE" ]; then 
@@ -14,4 +17,11 @@ if [ -n "$CONTAINER_EXISTS" ]; then
   sudo docker rm "$CONTAINER_NAME"
 fi
 
-sudo docker run -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock -d --name "$CONTAINER_NAME" "$IMAGE_NAME"
+# Add this line to create a new group inside the container with the same GID as the Docker group on the host
+sudo groupadd -g $DOCKER_GROUP_GID docker-host
+
+# Add this line to add the user inside the container to the newly created group
+sudo usermod -aG docker-host $EC2_USERNAME
+
+# Now, you should be able to run Docker commands inside the container without permission issues
+sudo docker run -p 8080:8080 -d --name "$CONTAINER_NAME" "$IMAGE_NAME"
